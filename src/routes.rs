5241f21a7ee0899e7ae2b66036e0083c176a07fb
hota822 <hota822@gmail.com>
2020-06-19 14:44:39 +0900

    cron test 2

--- src/routes.rs
+++ src/routes.rs
@@ -17,8 +17,8 @@ pub fn webhook(signature: Signature, body: Body) -> Result<(), &'static str> {
     controllers::reply::construct(signature, body, function)
 }
 
-#[post("/everyday", format = "application/json", data = "<body>")]
 // pub fn cron(signature: Signature, body: Body) -> Result<(), &'static str> {
+#[post("/everyday", format = "application/json", data = "<body>")]
 pub fn cron(signature: Signature, body: Body) -> &'static str {
     // controllers::push::construct(signature, body)
     println!("everyday received request.");
