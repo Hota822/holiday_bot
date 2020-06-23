3e3f8ea0647762a10afa0bc297c37748b491eeac
hota822 <hota822@gmail.com>
2020-06-23 11:59:38 +0900

    temp: change environment form wsl to docker

--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -4,10 +4,18 @@ use std::env;
 
 const CHANNEL_SECRET: &str = "CHANNEL_SECRET";
 const CHANNEL_ACCESS_TOKEN: &str = "CHANNEL_ACCESS_TOKEN";
+const USER_ID: &str = "USER_ID";
 
 fn get_secret_data() -> Result<(String, String), env::VarError> {
     let secret = env::var(CHANNEL_SECRET)?;
     let token = env::var(CHANNEL_ACCESS_TOKEN)?;
     Ok((secret, token))
+}
+
+fn get_user() -> Result<Vec<String>, env::VarError> {
+    // get target user from db
+    let user_ids = env::var(USER_ID)?;
+    let user_ids = user_ids.split(",").map(|s| s.to_string()).collect::<Vec<String>>();
+    Ok(user_ids)
 }
 
--- src/controllers/push.rs
+++ src/controllers/push.rs
@@ -6,7 +6,7 @@ use crate::public_holidays as ph;
 // use crate::utils::holiday;
 use crate::utils::holiday as date;
 
-use super::get_secret_data;
+use super::{get_secret_data, get_user};
 
 const SECRET: &str = "hota822ss";
 
@@ -27,25 +27,14 @@ fn push() {
         Err(_) => ("failure".to_string(), "failure".to_string() ),
     };
     let bot = bot::LineBot::new(&secret, &token);
-    let tomorrow = date::tomorrow();
-    let next_month = date::next_month();
+    if let  Some(messages) = everyday_check("public") {
+        // bot.push_multicast_messages(to: &vec, msg: Vec<LineMessage>)
+    };
+    if let  Some(messages) = indivisual_check() {
+        // bot.push_messages(to: &vec, msg: Vec<LineMessage>)
+    };
 
-    // let today = Local::today().naive_local();
-    // get first data in database holiday
-    // if first holiday == tomorrow  {
-        // line push
-    // }
-    // if first holiday == next_month {
-        // line push
-    // }
-    // get personal day in database personal_holiday
-    // if first holiday is next day {
-
-    // }
-    // if first holiday is two weeks (or 1 month?) later {
-
-    // }
-
+        // end
 
 
 
@@ -83,6 +72,33 @@ fn create_msg_for_text(data: Value) -> messages::LineMessage {
         }
         messages::LineMessage::create_text("", &message)
     }
+}
+
+fn everyday_check(db_name: &str ) -> Option<String> {
+    let tomorrow = date::tomorrow();
+    let next_month = date::next_month();
+    let data = match db_name {
+        "public" => panic!(), // db(public_holidays).get()
+        "indivisual" => panic!(), //db(indivisual_holidays).get()
+        _ => unreachable!(), // i want to return none, but it too much hassle
+    };
+    let mut messages = "".to_string();
+    // let today = Local::today().naive_local();
+    // data.get first data in database holiday
+    // if first holiday == tomorrow  {
+        // messages.push_str(create message)
+    // }
+    // if first holiday == next_month {
+        // messages.push_str(create message)
+    // }
+    if messages.len() == 0 {
+        return None
+    }
+    Some(messages)
+}
+
+fn indivisual_check() -> Option<String> {
+    Some("ok".to_string())
 }
 
 fn not_supported(data: Value) -> messages::LineMessage {
